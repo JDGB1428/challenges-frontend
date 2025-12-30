0dec41f532f5b5f91bb7d800ea620c32da9b0890
JDGB1428 <juangalindo28@hotmail.com>
2025-12-30 07:59:30 -0500

    Agregar: datos de para mostrar en el sidebar

---
+++ src/data/sidebarItem.ts
@@ -0,0 +1,14 @@
+export const sidebarItems = [
+    { key: "Home", label: "Home", icon: "home" as const },
+    { key: "Impacto Social", label: "Impacto Social", icon: "heart" as const },
+    { key: "Comunidad", label: "Comunidad", icon: "users" as const },
+    { key: "Sponsors", label: "Sponsors", icon: "sparkles" as const },
+    { key: "Marketplace", label: "Marketplace", icon: "store" as const },
+    { key: "Bakanes", label: "Bakanes", icon: "folder" as const },
+    { key: "Contenidos", label: "Contenidos", icon: "folder" as const },
+    {
+        key: "Categorias de acciones",
+        label: "Categorias de acciones",
+        icon: "folder" as const,
+    },
+];
